5c486b636eaeaf4ae581a1a61e2a0b104227512a
Ragan Martinez <ragan.martinez@live.com>
2020-03-18 01:40:23 +1100

    Layout styles for home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { NavigationComponent } from './components/navigation/navigation.component';
 import { MoviesComponent } from './components/movies/movies.component';
 import { MovieComponent } from './components/movie/movie.component';
 import { CinemasComponent } from './components/cinemas/cinemas.component';
 import { CinemaComponent } from './components/cinema/cinema.component'
 
 const routes: Routes = [
+  { path: '', component: NavigationComponent },
   { path: 'movies', component: MoviesComponent },
   { path: 'movie/:id', component: MovieComponent },
   { path: 'cinemas', component: CinemasComponent },
