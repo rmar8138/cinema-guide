7038fd2dbce37cda161f62c263cddead72e1c987
Ragan Martinez <ragan.martinez@live.com>
2020-03-17 20:59:40 +1100

    Render all cinemas to cinemas component

--- src/app/components/cinemas/cinemas.component.ts
+++ src/app/components/cinemas/cinemas.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MovieService } from './../../services/movie.service';
+
+import { Movie } from './../../models/Movie';
+import { Cinema } from './../../models/Cinema';
 
 @Component({
   selector: 'app-cinemas',
@@ -6,10 +10,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cinemas.component.scss']
 })
 export class CinemasComponent implements OnInit {
+  movies: Movie[];
+  cinemas: Cinema[];
 
-  constructor() { }
+  constructor(
+    private movieService: MovieService
+  ) { }
 
   ngOnInit(): void {
+    this.movies = this.movieService.getMovies();
+    this.cinemas = this.movieService.getCinemas();
   }
 
 }
--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { MovieService } from './../../services/movie.service';
 
 import { Movie } from './../../models/Movie';
-import { Showing } from './../../models/Showing';
 import { Cinema } from './../../models/Cinema';
 
 @Component({
@@ -12,14 +11,14 @@ import { Cinema } from './../../models/Cinema';
 })
 export class MoviesComponent implements OnInit {
   movies: Movie[];
-  showings: Showing[];
   cinemas: Cinema[];
 
-  constructor(private movieService: MovieService) { }
+  constructor(
+    private movieService: MovieService
+  ) { }
 
   ngOnInit(): void {
     this.movies = this.movieService.getMovies();
-    this.showings = this.movieService.getShowings();
     this.cinemas = this.movieService.getCinemas();
   }
 
